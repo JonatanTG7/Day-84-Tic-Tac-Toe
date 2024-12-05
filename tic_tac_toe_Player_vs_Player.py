ddda4537e2a19fc2329ac23fde0c730f08b5e896
Jonatan Gelerstain <jonatangeler7@gmail.com>
2024-12-05 13:03:07 +0200

    final Player vs Player

--- tic_tac_toe_Player_vs_Player.py
+++ tic_tac_toe_Player_vs_Player.py
@@ -16,66 +16,69 @@ game_turn =["X" , "O"]
 #Number of rows and columns
 row_column_index =["0" ,"1", "2"]
 
-#
+#printing the borad with the X & O
 def print_board():
     print("\nTic_Tac_Toe_Game:\n")
 
-    #Creating the board
+    #Creating the board.
     for index,row in enumerate(board):
         print("  |".join(row))
         if index != 2:
             print("-----------")
-     
+
+#checking if the row and column that the user is entering is corrcet and free.
 def player_turn(player):
     valid_move =False
     while not valid_move:
         print(f"\nTurn of {player} :")
+
         row = input("Please enter a row between 0-2: ")
         column = input("Please enter a column between 0-2: ")
-        if row not in row_column_index:
-            print(f"You entered {row} and this is wrong. enter 0, 1 or 2")
-        elif column not in row_column_index:
-            print(f"You entered {column} and this is wrong. enter 0, 1 or 2")
+
+        if row not in row_column_index or column not in row_column_index:
+            print(f"Invalid input! Please enter a valid input 0, 1 or 2 to row and column. ")
         elif board[int(row)][int(column)] != " ":
-            print("The row and column you enter is not free, try a diffrent place ")
+            print("The row and column you enter is not free, try a diffrent place. ")
         else:
             board[int(row)][int(column)] = f"{player}"
-            print_board()
             valid_move = True
+            print_board()
             
-
-def check_winner(player,game_draw):
-    print_message = False
+#checking if one of the users won the game.
+def check_winner(player,count_turns):
+    is_winner = False
     for i in range(len(board)):
-        if board[i][0] == board[i][1] == board[i][2] and board[i][2] != " ":
-            print_message = True
+        if board[i][0] == board[i][1] == board[i][2] == player:
+            is_winner = True
     for j in range(len(board[0])):
-        if board[0][j] == board[1][j] == board[2][j] and board[2][j] != " ":
-            print_message = True
-    if board[0][0] == board[1][1] == board[2][2] and board[2][2] != " ":
-            print_message = True
-    if board[2][0] == board[1][1] == board[0][2] and board[0][2] != " ":
-            print_message = True
-    if print_message:
+        if board[0][j] == board[1][j] == board[2][j] == player:
+            is_winner = True
+
+    if all(board[i][i] == player for i in range(3)) or all([board[i][2 - i] == player for i in range(3)]): 
+        is_winner = True
+
+    if is_winner:
         print(f"The player {player} won! ")
         return True
-    if game_draw == 8:
+    
+    if count_turns == 8:
         print("It is a draw :)")
         return True
     return False
-        
-def game():
+
+#main func 
+def start_game():
     print_board()
     winning = False
-    game_draw = 0
+    count_turns = 0
     current_player = game_turn[randint(0,1)]
     while not winning:
         player_turn(current_player)
-        winning = check_winner(current_player,game_draw)
-        game_draw += 1
+        winning = check_winner(current_player,count_turns)
+        count_turns += 1
         if current_player == "X":
             current_player = "O"
         else:
             current_player = "X"
 
-game()
+start_game()
