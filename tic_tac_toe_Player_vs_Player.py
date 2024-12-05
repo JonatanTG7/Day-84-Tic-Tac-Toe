bc4db197e4834d5d6119cc7832cf03b70fe4b64f
Jonatan Gelerstain <jonatangeler7@gmail.com>
2024-12-05 13:14:09 +0200

    added code notes

--- tic_tac_toe_Player_vs_Player.py
+++ tic_tac_toe_Player_vs_Player.py
@@ -16,7 +16,7 @@ game_turn =["X" , "O"]
 #Number of rows and columns
 row_column_index =["0" ,"1", "2"]
 
-#printing the borad with the X & O
+#Printing the borad with the X & O
 def print_board():
     print("\nTic_Tac_Toe_Game:\n")
 
@@ -26,56 +26,78 @@ def print_board():
         if index != 2:
             print("-----------")
 
-#checking if the row and column that the user is entering is corrcet and free.
+#Checking if the row and column that the user is entering is correct and free.
 def player_turn(player):
     valid_move =False
     while not valid_move:
         print(f"\nTurn of {player} :")
 
+        #Input from user
         row = input("Please enter a row between 0-2: ")
         column = input("Please enter a column between 0-2: ")
 
+        #Checks if the input is valid (0 , 1 or 2)
         if row not in row_column_index or column not in row_column_index:
             print(f"Invalid input! Please enter a valid input 0, 1 or 2 to row and column. ")
+        #Checks if the cell is free
         elif board[int(row)][int(column)] != " ":
             print("The row and column you enter is not free, try a diffrent place. ")
         else:
+            #Add the symbol of the player to the board
             board[int(row)][int(column)] = f"{player}"
             valid_move = True
             print_board()
             
-#checking if one of the users won the game.
+#Checking if one of the users won the game.
 def check_winner(player,count_turns):
     is_winner = False
+    #Checks if we have a same symbol in the row
     for i in range(len(board)):
         if board[i][0] == board[i][1] == board[i][2] == player:
             is_winner = True
+    #Checks if we have a same symbol in the column
     for j in range(len(board[0])):
         if board[0][j] == board[1][j] == board[2][j] == player:
             is_winner = True
 
+    #Checks if we have a same symbol in the diagonal
     if all(board[i][i] == player for i in range(3)) or all([board[i][2 - i] == player for i in range(3)]): 
         is_winner = True
 
+    #Checks if I won
     if is_winner:
         print(f"The player {player} won! ")
         return True
     
-    if count_turns == 8:
+    #Checks if draw after 9 moves
+    if count_turns == 9:
         print("It is a draw :)")
         return True
     return False
 
-#main func 
+#Main func 
 def start_game():
+
+    #Printing the board before starting
     print_board()
+
+    #For while loop
     winning = False
-    count_turns = 0
+    #Couning moves
+    count_turns = 1
+    #Random player start
     current_player = game_turn[randint(0,1)]
+    #While until win or draw
     while not winning:
+        #Player turn
         player_turn(current_player)
-        winning = check_winner(current_player,count_turns)
+        #Checks if we have a winner
+        if count_turns <= 5:
+            winning = check_winner(current_player,count_turns)
+        #move ++
         count_turns += 1
+
+        #Changing player turn
         if current_player == "X":
             current_player = "O"
         else:
